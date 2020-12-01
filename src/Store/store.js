675aa7d399b08369ee22bf1b122aa937696706eb
Rusdiv <64499427+Rusdiv@users.noreply.github.com>
2020-12-01 21:27:44 +0300

    store refactoring

--- src/Store/store.js
+++ src/Store/store.js
@@ -8,5 +8,4 @@ const reducers = combineReducers({
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers());
 
-window.store = store;
 export default store;
